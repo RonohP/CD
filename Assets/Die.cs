828efa739387998d00e91d1db11729f24c5a286c
RonohP <ronopurity05@gmail.com>
2021-05-31 14:14:35 +0300

    update

---
+++ Assets/Die.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
